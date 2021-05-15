b8fce0c0f06649bf252d2401ff7a92ef06722e48
Ting-Chi Wong <tcwong007@gmail.com>
2021-05-15 13:30:43 +0200

    Iterations 1 to 6 done.

--- app.js
+++ app.js
@@ -1,60 +1,65 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
+const MONGODB_URI = "mongodb+srv://public-user:admin2021@cluster0.jcdmn.mongodb.net/lab-mongoose-movies-2?retryWrites=true&w=majority";
 
 mongoose
-  .connect('mongodb://localhost/lab-mongoose-movies2', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .connect(MONGODB_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
   });
-
 
 const app = express();
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true,
+  })
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
 //
 // Routers
 //
 
-const indexRouter = require('./routes/index.js');
-app.use('/', indexRouter);
+const indexRouter = require("./routes/index.js");
+app.use("/", indexRouter);
 
-const celebritiesRouter = require('./routes/celebrities.js');
-app.use('/celebrities', celebritiesRouter);
+const celebritiesRouter = require("./routes/celebrities.js");
+app.use("/celebrities", celebritiesRouter);
 
 module.exports = app;
--- bin/seeds.js
+++ bin/seeds.js
@@ -1,12 +1,18 @@
-const mongoose = require('mongoose');
-const Celebrity = require('../models/celebrity.js');
+const mongoose = require("mongoose");
+const Celebrity = require("../models/celebrity.js");
 
-mongoose.connect('mongodb://localhost/lab-mongoose-movies2', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
-  .then(() => {
-    console.log('🔌 Connected to Mongo!');
+const MONGODB_URI = "mongodb+srv://public-user:admin2021@cluster0.jcdmn.mongodb.net/lab-mongoose-movies-2?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
   })
-  .catch(err => console.error('Error connecting to mongo', err))
-;
+  .then(() => {
+    console.log("🔌 Connected to Mongo!");
+  })
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 //
 // Celebrities
@@ -14,20 +20,22 @@ mongoose.connect('mongodb://localhost/lab-mongoose-movies2', {useNewUrlParser: t
 
 var datas = [
   {
-      name: 'Kim Kardashian',
-      occupation: 'Unknown',
-      catchPhrase: "I'm a vegan",
+    name: "Kim Kardashian",
+    occupation: "Unknown",
+    catchPhrase: "I'm a vegan",
   },
   {
-      name: 'Melz',
-      occupation: 'unemployed',
-      catchPhrase: "Is this a thing?",
+    name: "Melz",
+    occupation: "unemployed",
+    catchPhrase: "Is this a thing?",
   },
   {
-      name: 'Patrick Star',
-      occupation: 'Krusty Krab',
-      catchPhrase: "Noooo, this is PATRICK",
-  }
+    name: "Patrick Star",
+    occupation: "Krusty Krab",
+    catchPhrase: "Noooo, this is PATRICK",
+  },
 ];
 const p1 = Celebrity.create(datas);
-p1.then(celebrities => console.log(`${celebrities.length} celebrities created!`))
+p1.then((celebrities) =>
+  console.log(`${celebrities.length} celebrities created!`)
+);
